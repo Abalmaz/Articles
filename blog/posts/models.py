968f9aecea0a2cccf7ed31d5893c8ce9a5550cf2
abalmazik <yana.abalmaz@gmail.com>
2018-07-07 22:14:50 +0300

    Initial commit

---
+++ blog/posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=20)
+    description = models.CharField(max_length=250)
+
+
+class Post(models.Model):
+
+    STATUS_REVIEW = 0
+    STATUS_LIVE = 100
+
+    STATUSES = (
+        (STATUS_REVIEW, 'Review'),
+        (STATUS_LIVE, 'Live')
+    )
+
+    status = models.SmallIntegerField(choices=STATUSES, default=STATUS_REVIEW)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
